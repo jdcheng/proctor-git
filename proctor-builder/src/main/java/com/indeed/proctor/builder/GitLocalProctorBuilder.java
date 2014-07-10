12c72f503c3d6a02946ffd1d1ce2031894480cb1
John Cheng <jcheng@homeaway.com>
2014-07-10 17:43:49 -0500

    added proctor-store-git to pom and created a proctor builder for git

---
+++ proctor-builder/src/main/java/com/indeed/proctor/builder/GitLocalProctorBuilder.java
@@ -0,0 +1,74 @@
+package com.indeed.proctor.builder;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.log4j.Logger;
+
+import com.indeed.proctor.common.IncompatibleTestMatrixException;
+import com.indeed.proctor.store.GitProctor;
+import com.indeed.proctor.store.ProctorReader;
+import com.indeed.proctor.store.StoreException;
+
+public class GitLocalProctorBuilder extends ProctorBuilder {
+
+    private static final Logger LOGGER = Logger.getLogger(GitLocalProctorBuilder.class);
+
+    public GitLocalProctorBuilder(ProctorReader proctorReader, Writer outputSink) {
+        super(proctorReader, outputSink);
+    }
+
+    public GitLocalProctorBuilder(ProctorReader proctorReader, Writer outputSink, String author) {
+        super(proctorReader, outputSink, author);
+    }
+
+    public GitLocalProctorBuilder(ProctorReader proctorReader, Writer outputSink, String author, String version) {
+        super(proctorReader, outputSink, author, version);
+    }
+
+    private static class GitLocalProctorBuilderArgs extends ProctorBuilderArgs {
+        private String inputGitUrl;
+
+        private GitLocalProctorBuilderArgs() {
+            options.addOption(OptionBuilder.hasArg(true)
+                    .isRequired()
+                    .withLongOpt("input")
+                    .withArgName("input directory")
+                    .withDescription("The directory to read from.")
+                    .create("i"));
+        }
+
+        @Override
+        protected void extract(CommandLine results)  {
+            super.extract(results);
+            this.inputGitUrl = results.getOptionValue("input");
+        }
+
+        public String getInputGitUrl() {
+            return inputGitUrl;
+        }
+    }
+
+    public static void main(final String[] args) throws IOException, StoreException, IncompatibleTestMatrixException {
+        final GitLocalProctorBuilderArgs arguments = new GitLocalProctorBuilderArgs();
+        arguments.parse(args);
+
+        try {
+            new GitLocalProctorBuilder(
+                    new GitProctor(arguments.getInputGitUrl(), "", ""),
+                    "-".equals(arguments.getOutputdir()) ?
+                            new PrintWriter(System.out) :
+                            new FileWriter(new File(arguments.getOutputdir(), arguments.getFilename())),
+                    arguments.getAuthor(),
+                    arguments.getVersion()).execute();
+        } catch (Exception e) {
+            LOGGER.error("Failed to generates proctor artifact from " + arguments.getInputGitUrl(), e);
+            System.exit(1);
+        }
+    }
+}
